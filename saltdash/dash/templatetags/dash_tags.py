272307ad661713e1cfe418eb98d0b4a6311235c9
Peter Baumgartner <pete@lincolnloop.com>
2018-01-30 13:01:54 -0700

    Fixes pagination links

--- saltdash/dash/templatetags/dash_tags.py
+++ saltdash/dash/templatetags/dash_tags.py
@@ -1,4 +1,5 @@
 import json
+from urllib.parse import urlencode
 
 from django.template import Library
 
@@ -17,3 +18,11 @@ def get_minions():
     return (Result.objects.order_by('minion')
                           .values_list('minion', flat=True)
                           .distinct())
+
+
+@register.simple_tag(takes_context=True)
+def url_replace(context, **kwargs):
+    """Replace a single GET parameter while maintaining others"""
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
