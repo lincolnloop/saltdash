2e17e60f5ece18a9b79e235279436571e7cde9ac
Peter Baumgartner <pete@lincolnloop.com>
2018-01-31 11:55:55 -0700

    Limit pages displayed

--- saltdash/dash/templatetags/dash_tags.py
+++ saltdash/dash/templatetags/dash_tags.py
@@ -40,3 +40,38 @@ def pretty_time(ms):
         return '{:.2f} sec'.format(ms/1000)
     # pad one character right to keep alignment in states
     return mark_safe('{:.0f} ms&nbsp;'.format(round(ms, 0)))
+
+
+@register.filter
+def clipped_page_range(page_range: range, current_page: int) -> list:
+    """Abbreviated page range to avoid a massive pagination"""
+    page_range = list(page_range)
+    if len(page_range) <= 8:
+        return page_range
+    # show first two and last two pages
+    start = page_range[:2]
+    end = page_range[-2:]
+
+    # prevent middle from going beyond start/end
+    if current_page >= start[0] + 2:
+        first_middle = current_page - 2
+    else:
+        first_middle = start[0]
+    if current_page <= end[-1] - 2:
+        last_middle = current_page + 2
+    else:
+        last_middle = end[-1]
+    middle = list(range(first_middle, last_middle + 1))
+
+    # add gaps in pages as None if necessary
+    if middle[0] > start[-1] + 1:
+        pages = start + [None] + middle
+    else:
+        pages = list(set(start + middle))
+    if middle[-1] + 1 > end[0]:
+        for p in end:
+            if pages[-1] < p:
+                pages.append(p)
+    else:
+        pages = pages + [None] + end
+    return pages
--- saltdash/dash/tests/test_templatetags.py
+++ saltdash/dash/tests/test_templatetags.py
@@ -1,10 +1,10 @@
 from decimal import Decimal
 from unittest import TestCase
 
-from saltdash.dash.templatetags.dash_tags import pretty_time
+from saltdash.dash.templatetags.dash_tags import pretty_time, clipped_page_range
 
 
-class DashTagsTestCase(TestCase):
+class PrettyTimeTestCase(TestCase):
 
     def test_minutes(self):
         self.assertEqual(pretty_time(69305.23),
@@ -17,3 +17,23 @@ class DashTagsTestCase(TestCase):
     def test_milliseconds(self):
         self.assertEqual(pretty_time(305.23),
                          '305 ms&nbsp;')
+
+
+class ClippedPageRangeTestCase(TestCase):
+
+    def test_long(self):
+        clipped = clipped_page_range(list(range(1, 21)), 10)
+        self.assertListEqual(clipped,
+                             [1, 2, None, 8, 9, 10, 11, 12, None, 19, 20])
+
+    def test_short(self):
+        clipped = clipped_page_range(list(range(1, 8)), 5)
+        self.assertListEqual(clipped, [1, 2, 3, 4, 5, 6, 7])
+
+    def test_left(self):
+        clipped = clipped_page_range(list(range(1, 21)), 2)
+        self.assertListEqual(clipped, [1, 2, 3, 4, None, 19, 20])
+
+    def test_right(self):
+        clipped = clipped_page_range(list(range(1, 21)), 18)
+        self.assertListEqual(clipped, [1, 2, None, 16, 17, 18, 19, 20])
